69ae09c6f30f898ddd09a95b8cebaff3992abf6d
Ryan Bazzana <ryan.bazzana@gmail.com>
2019-01-13 20:14:03 +1000

    Use new font, implement smooth scrolling

--- src/App.vue
+++ src/App.vue
@@ -20,10 +20,13 @@ export default {
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Open Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+html {
+  scroll-behavior: smooth;
 }
 </style>
