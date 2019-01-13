04988fc0d56e7e72f8e0a9e9aa09833fa5049e47
Ryan Bazzana <ryan.bazzana@gmail.com>
2019-01-13 14:41:49 +1000

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="header">
+    <div class="header__left">
+      <h1>Ryan Bazzana</h1>
+    </div>
+    <div class="header__right">
+      <ul>
+        <li>
+          <a href="#about">About me</a>
+        </li>
+        <li>
+          <a href="#about">Tech</a>
+        </li>
+        <li>
+          <a href="#about">Interests</a>
+        </li>
+        <li>
+          <a href="#about">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      scrolled: false
+    };
+  },
+  methods: {
+    handleScroll() {
+      this.scrolled = window.scrollY > 0;
+    }
+  },
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+};
+</script>
+
+<style lang="scss">
+.header {
+  display: block;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  background: #75a68e;
+  height: 125px;
+
+  &__left {
+    position: relative;
+    height: 100%;
+    display: inline-block;
+    width: 40%;
+  }
+  &__right {
+    ul {
+      list-style-type: none;
+      padding: 0;
+    }
+    li {
+      display: inline-block;
+      margin: 0 10px;
+    }
+    position: relative;
+    height: 100%;
+    display: inline-block;
+    width: 60%;
+    a {
+      color: #000;
+    }
+  }
+}
+</style>
