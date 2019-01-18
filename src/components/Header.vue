3bd55eee81bdca542bf2d8377d2bba99ac8072b1
Ryan Bazzana <ryan.bazzana@gmail.com>
2019-01-18 20:40:54 +1000

    Adjust header yet again

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,8 @@
 <template>
   <transition name="heightChange">
     <div class="header" :class="scrolled ? 'header--scrolled' : ''">
-      <div class="header__photo">
-        <img src>
-      </div>
       <div class="header__left">
-        <h1>Ryan Bazzana</h1>
+        <h3 class="header__me">Ryan Bazzana</h3>
       </div>
       <div class="header__right">
         <ul>
@@ -49,6 +46,7 @@ export default {
 .header {
   display: none;
   @media screen and (min-width: 768px) {
+    height: auto;
     display: block;
     position: fixed;
     top: 0;
@@ -56,24 +54,30 @@ export default {
     right: 0;
     max-width: 100%;
     margin: 0 auto;
-    background-color: rgba(0, 0, 0, 0.3);
-    color: #fff;
+    background-color: #e8e8e8;
+    border-top: 1px solid #c8c8c8;
+    border-bottom: 1px solid #c8c8c8;
+    color: #000;
     z-index: 5;
     div {
       vertical-align: middle;
     }
   }
-  &__photo {
-    display: inline-block;
-    img {
-      position: relative;
-      width: 100px;
-      height: 100px;
-      border-radius: 50%;
-      display: inline-block;
-      margin: 0px;
-    }
+  &__me {
+    vertical-align: middle;
+    margin: 0px;
   }
+  // &__photo {
+  //   display: inline-block;
+  //   img {
+  //     position: relative;
+  //     width: 100px;
+  //     height: 100px;
+  //     border-radius: 50%;
+  //     display: inline-block;
+  //     margin: 0px;
+  //   }
+  // }
   &__left {
     display: none;
     @media screen and (min-width: 768px) {
@@ -101,31 +105,15 @@ export default {
     }
     a {
       font-size: 2vw;
-      color: #919191;
+      color: #000;
       transition: all 0.3s ease;
       text-decoration: none;
       &:hover {
-        color: #fff;
+        color: #000;
         font-size: 2vw;
         text-decoration: underline;
       }
     }
   }
-}
-.header--scrolled {
-  position: fixed;
-  height: 75px;
-  top: 0;
-  left: 0;
-  right: 0;
-}
-.heightChange-enter-active,
-.heightChange-leave-active {
-  transition: height 1s ease-in-out;
-  overflow: hidden;
-}
-.heightChange-enter,
-.heightChange-leave-to {
-  height: auto;
 }
 </style>
